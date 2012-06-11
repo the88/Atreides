275adf7367f56ea880d77965d17f59387b50576d
Benjamin Manns <benmanns@gmail.com>
2012-06-11 15:55:22 -0400

    Fix typo in asset path for OrdersHelper#items_column.

--- app/helpers/admin/atreides/orders_helper.rb
+++ app/helpers/admin/atreides/orders_helper.rb
@@ -16,7 +16,7 @@ module Admin::Atreides::OrdersHelper
     order.line_items.map{|li|
       photo = li.product.photos.first
       link_to(
-        image_tag(photo ? photo.image.url(:thumb) : "atriedes/admin/product.png", :size => "50x50"), admin_product_path(li.product),
+        image_tag(photo ? photo.image.url(:thumb) : "atreides/admin/product.png", :size => "50x50"), admin_product_path(li.product),
         :title => "#{li.product.title} - #{li.size} x #{li.qty}"
       )
     }.join
